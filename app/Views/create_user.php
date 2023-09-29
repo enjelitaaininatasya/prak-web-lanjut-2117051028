68082f481687badea99a77966825e47200834c4e
enjelitaaininatasya <enjelitaaininatasyaa@gmail.com>
2023-09-29 10:43:58 +0700

    read data

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,12 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
+<?= $this-> extend('layouts/app')?>
+<?= $this-> section('content')?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <h1 class="h3 mt-5 mb-3 fw-normal" style="margin-top : 30px; margin-left : 35px;">Tambah Data</h1>
@@ -44,6 +37,4 @@
 
         <center><button class="btn btn-primary" type="submit" style="margin-top : 35px">Tambah</button></center>
     </form>
-
-</body>
-</html>
+<?= $this->endSection()?>
